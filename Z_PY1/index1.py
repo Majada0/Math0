305d6352a9879f8053ab91baab91832b1bd6f11d
Majada0 <86021632+Majada0@users.noreply.github.com>
2021-07-03 19:53:52 +0300

    Add files via upload

---
+++ Z_PY1/index1.py
@@ -0,0 +1,38 @@
+import math
+# Relational and Equality Operators
+a=30
+b=20
+if a==b:
+  print('a is equal to b')
+else:
+  print('a and b are not equal')
+
+
+
+# Relational and Equality Operators
+a=30
+b=20
+if a!=b:
+  print('a is not equal to b')
+else:
+  print('a and b are equal')
+  
+
+# Print the square of first 10 numbers ( 1 to 10)
+for i in range(1,10):
+
+  print(i*i)
+
+
+# Print the series of first 10 even numbers ( 20 to 2)
+for i in range(20,2,-2): 
+     print(i)
+
+
+     
+# Print the reverse given series  ( 50, 48, 46, 44, 42, 40)
+for i in range(50,40,-2): 
+     print(i)
+
+
+  
